00c4fddcd04f1aae0cf50f820ae5b0944791eb18
jmouer <jrmouer@gmail.com>
2016-07-21 13:36:35 -0400

    Add Organization Model

---
+++ BetterBet/BetterBet/Models/OrganizationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterBet.Models
+{
+    public class Organization
+    {
+        public string Name { get; set; }
+
+        public Guid Token { get; set; }
+    }
+}
