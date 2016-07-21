3da3af78a5ae6a11f48bc23ec8508da5bda26fe3
jmouer <jrmouer@gmail.com>
2016-07-21 12:49:42 -0400

    Add comment model

---
+++ BetterBet/BetterBet/Models/CommentModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterBet.Models
+{
+    public class CommentModels
+    {
+    }
+
+    public class Comment
+    {
+        public User user { get; set; } //I don't know what the user model looks like right now
+
+        public string content { get; set; }
+
+        public DateTime timestamp { get; set; }
+    }
+}
