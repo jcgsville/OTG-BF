c7162282935c78215b592d66c500eda3cbc370e4
Tyler.Goulding <tgouldin@andrew.cmu.edu>
2016-07-21 13:01:15 -0400

    adding usr and bet models

---
+++ BetterBet/BetterBet/Models/BetModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterBet.Models
+{
+    public class BetModel
+    {
+        public Guid betId { get; set; }
+        public string challenge { get; set; }
+        public List<offerModel> offerList { get; set; }
+        public List<Comment> commentList { get; set; }
+    }
+}
---
+++ BetterBet/BetterBet/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterBet.Models
+{
+    public class UserModel
+    {
+        public string name { get; set; }
+        public Guid cardToken { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public List<offerModel> offerList { get; set;}
+    }
+}
