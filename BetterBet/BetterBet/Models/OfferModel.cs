7a073dfcf0a6c1eea26c5d57f697c4e395d9ab7a
Marshall Clyburn <mardev2013@gmail.com>
2016-07-21 13:10:02 -0400

    Add OfferModel.

---
+++ BetterBet/BetterBet/Models/OfferModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterBet.Models
+{
+    public class OfferModel
+    {
+        public UserModel user { get; set; }
+        public UInt32 amount { get; set; }
+        public String charity { get; set; }
+        public String condition { get; set; }
+    }
+}
